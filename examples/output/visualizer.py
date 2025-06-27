1c0065a2a6f1cb7cc10be4c8701d54f7dcef7837
Navid Feizi <feizinavid94@gmail.com>
2025-06-27 15:49:31 -0400

    update

---
+++ examples/output/visualizer.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+
+plt.rcParams["grid.color"] = "lightgray"
+plt.rcParams["grid.linewidth"] = 0.5
+matplotlib.rc("font", family="serif", size=7)
+matplotlib.rcParams["text.usetex"] = True
+
+
+df = pd.read_csv("backbone.csv", delimiter=",")
+
+fig = plt.figure(figsize=(10, 8))
+gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+axs = []
+
+axs.append(fig.add_subplot(gs[0, 0]))      
+axs.append(fig.add_subplot(gs[1, 0], projection='3d'))   
+
+axs[0].plot(df["z"]*1e3, df["y"]*1e3, label="Backbone", color="blue")
+axs[0].set_aspect('equal')
+axs[0].set_title("Backbone")
+axs[0].set_xlabel("Z [mm]")
+axs[0].set_ylabel("Y [mm]")
+axs[0].grid("both")
+
+axs[1].plot(df["z"]*1e3, df["x"]*1e3, df["y"]*1e3, label="Backbone", color="red")
+axs[1].set_aspect('equal')
+axs[1].set_title("Backbone 3D")
+axs[1].set_xlabel("Z [mm]")
+axs[1].set_ylabel("X [mm]")
+axs[1].set_zlabel("Y [mm]")
+axs[1].grid(True)
+
+plt.tight_layout()
+
+plt.savefig(os.path.join(os.path.dirname(__file__), "backbone_plot.pdf"))
+
+plt.show(block=False)
+
+
+
+
---
+++ examples/tdcr_torch_backend.py
@@ -0,0 +1,107 @@
+import os, sys
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import rc
+from matplotlib.gridspec import GridSpec
+
+from tdcr_torch import TDCR_Robot
+
+# Set matplotlib configurations
+plt.rcParams["grid.color"] = "lightgray"
+plt.rcParams["grid.linewidth"] = 0.5
+matplotlib.rc("font", family="serif", size=7)
+matplotlib.rcParams["text.usetex"] = True
+rc("text", usetex=True)
+
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+# else:
+device = torch.device("cpu")
+torch.set_num_threads(12)
+
+
+def main():
+
+    E = 15.467e9  # E
+    G = 5.6e9  # G
+    radius = 0.0010  # radius
+    mass = 0.0040  # mass
+    length = 1.0  # length
+    tendon_offset = 1.112e-3  # tendon offset
+    num_tendons = 1
+
+    robot = TDCR_Robot(E, G, radius, mass, length, tendon_offset, num_tendons, device, 100)
+
+    tau = np.array([30.0])
+    robot.set_tendon_pull(tau)
+    Y = robot.scipy_solver()
+    # Y = robot.bvp_solve()
+
+    """ Plot """
+    fig = plt.figure(figsize=(8, 5))
+    gs = GridSpec(2, 2, figure=fig, height_ratios=[1, 1], width_ratios=[1, 2])
+    axs = []
+    axs.append(fig.add_subplot(gs[0, 0]))
+    axs.append(fig.add_subplot(gs[1, 0]))
+    axs.append(fig.add_subplot(gs[0:3, 1], projection="3d"))  # 3D plot
+
+    axs[0].plot(Y[2, :], Y[0, :], color="black", label="x")
+    axs[0].set_xlabel("z [m]")
+    axs[0].set_ylabel("x [m]")
+    axs[0].set_title("x")
+    axs[0].grid("both")
+
+    axs[1].plot(Y[2, :], Y[1, :], color="black", label="y")
+    axs[1].set_xlabel("z [m]") 
+    axs[1].set_ylabel("y [m]")
+    axs[1].set_title("y")
+    axs[1].grid("both")
+
+    axs[2].plot3D(Y[2, :], Y[0, :], Y[1, :], label="Backbone")
+    axs[2].scatter(0.0, 0.0, 0.0, color='red', label="base")
+    axs[2].set_xlabel("Z [m]")
+    axs[2].set_ylabel("X [m]")
+    axs[2].set_zlabel("Y [m]")
+    axs[2].set_title("3D Backbone")
+    axs[2].legend()
+    axs[2].set_box_aspect([1, 1, 1])
+
+    # Equal aspect ratio: set data limits to be the same
+    xdata = Y[2, :]
+    ydata = Y[0, :]
+    zdata = Y[1, :]
+
+    max_range = (
+        np.array(
+            [
+                xdata.max() - xdata.min(),
+                ydata.max() - ydata.min(),
+                zdata.max() - zdata.min(),
+            ]
+        ).max()
+        / 2.0
+    )
+
+    mid_x = (xdata.max() + xdata.min()) / 2
+    mid_y = (ydata.max() + ydata.min()) / 2
+    mid_z = (zdata.max() + zdata.min()) / 2
+
+    axs[2].set_xlim(mid_x - max_range, mid_x + max_range)
+    axs[2].set_ylim(mid_y - max_range, mid_y + max_range)
+    axs[2].set_zlim(mid_z - max_range, mid_z + max_range)
+
+    plt.tight_layout()
+    plt.draw()
+    plt.show(block=True)
+    plt.pause(0.1)
+    plt.savefig(
+        os.path.join(os.path.dirname(__file__), "data", "tdcr_states.png"),
+        format="png",
+        dpi=600,
+    )
+
+
+if __name__ == "__main__":
+    main()
